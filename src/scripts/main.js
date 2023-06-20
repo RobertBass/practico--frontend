6bee34f583b863ff4aaa51939feb03c2168026c8
Roberto Bravo <robertbass@hotmail.com>
2023-06-20 00:14:12 -0500

    20/06/2023

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,43 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const hamMenu = document.querySelector('.menu')
-const shopMenu = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
-const cardsContainer = document.querySelector('.cards-container')
+let menuEmail = document.querySelector('.navbar-email');
+let desktopMenu = document.querySelector('.desktop-menu');
+let mobileMenu = document.querySelector('.mobile-menu');
+let hamMenu = document.querySelector('.menu')
+let shopMenu = document.querySelector('.navbar-shopping-cart');
+let closeIcon = document.querySelector('.product-detail-close');
+let shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+let cardsContainer = document.querySelector('.cards-container');
+let body = document.querySelector("body");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamMenu.addEventListener('click', toggleMobileMenu);
 shopMenu.addEventListener('click', toggleShopMenu);
 
 function toggleDesktopMenu(){
-    const isShopMenuClosed = aside.classList.contains('inactive');
+    const isShopMenuClosed = shoppingCartContainer.classList.contains('inactive');
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
     if(!isShopMenuClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     } else if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
+    } else if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isShopMenuClosed = aside.classList.contains('inactive');
+    const isShopMenuClosed = shoppingCartContainer.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
     if(!isShopMenuClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     } else if(!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive');
-    } 
+    } else if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
 
     mobileMenu.classList.toggle('inactive');
 }
@@ -37,32 +45,96 @@ function toggleMobileMenu(){
 function toggleShopMenu(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
     if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     } else if(!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive');
+    } else if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
+}
+
+function openDetail(product){
+  let isThereActiveProduct =
+    document.querySelectorAll("#productDetail").length > 0;
+  if (isThereActiveProduct) {
+    document.querySelectorAll("#productDetail").forEach((element) => {
+      element.remove();
+    });
+  }
+
+  let productDetailContainer = document.createElement('aside');
+  productDetailContainer.id = 'productDetail';
+  productDetailContainer.classList.add('aside-content');
+  productDetailContainer.innerHTML = `<div class="product-detail-close" onclick="removeProductDetail()">
+    <img src="./src/icons/icon_close.png" alt="close" />
+    </div>`;
+
+  let img = document.createElement('img');
+  img.src = product.img;
+  img.alt = product.name;
+
+  let productInfoContainer = document.createElement('div');
+  productInfoContainer.classList.add('product-info');
+  productInfoContainer.innerHTML = `<p>$ ${product.price}</p><p>${product.name}</p><p>${product.detail}</p>`
+
+  let btn = document.createElement('button');
+  btn.classList.add('primary-button', 'add-to-cart-button');
+  btn.onclick = function(){addToShoppingCart(product, btn)};
+  btn.innerHTML = `<img src="./src/icons/bt_add_to_cart.svg" alt="add" /> <p>Add to Cart</p>
+                    <span class="item-to-cart"></span>`;
+
+  productDetailContainer.append(img, productInfoContainer, btn);
+
+  if(window.innerWidth > 992){
+    cardsContainer.classList.add("cards-container--aside-open");
+  } else{
+    body.classList.add("no-scroll");
+  }
+  body.append(productDetailContainer);
+
+  //productDetailContainer.classList.remove("inactive");
+}
+
+function removeProductDetail(){
+    body.classList.remove("no-scroll");
+    
+    let productResume = document.querySelector("#productDetail");
+    productResume.remove();
+    cardsContainer.classList.remove("cards-container--aside-open");
+  }
+
+function closeDetail(){
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
 productList.push({
     name: 'Bike',
     price: 120,
-    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    detail: `With its practical position, this bike also fulfills a decorative,
+     as your hall or workspace`,
+     category: 'Others'
 });
 
 productList.push({
     name: 'Televisor',
     price: 500,
-    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    detail: 'TV 60 " Smart TV',
+    category: 'Electronics'
 });
 
 productList.push({
     name: 'Computadora',
     price: 1200,
-    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    detail: 'Intel Core i9 11th generation, 32GB RAM, SSD 1TB',
+    category: 'Electronics'
 });
 
 function renderProducts(arr){
@@ -72,6 +144,8 @@ function renderProducts(arr){
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.img);
+        productImg.setAttribute('class', 'prod');
+        productImg.onclick = function(){openDetail(product);};
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -104,6 +178,8 @@ function renderProducts(arr){
 }
 
 renderProducts(productList);
+
+
 
 
 
